1050e2ffd697a07d613cabcf2083d221ce963b6a
zmiaa <zmiao0826@gmail.com>
2025-09-29 17:05:17 +0200

    5.2 provat (no estava segura si el statement el cambiavem pel fetRentalPrice)

--- src/Customer.java
+++ src/Customer.java
@@ -4,43 +4,35 @@ import java.util.List;
 public class Customer {
     private final String name;
 
-    private final List<Rental> rentals = new ArrayList<>();
+    final List<Rental> rentals = new ArrayList<>();
 
     public Customer(String name) {
         this.name = name;
     }
 
-    public void addRental(Rental rental) {
-        rentals.add(rental);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String statement() {
+    public static String statement(Customer customer) {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        String result = "Rental Record for " + getName() + "\n";
+        String result = "Rental Record for " + customer.getName() + "\n";
 
-        for (Rental rental : this.rentals) {
-            double thisAmount = 0;
+        for (Rental rental : customer.rentals) {
+            double price = 0;
             Rental each = rental;
 
             // determines the amount for each line
             switch (each.getMovie().getPriceCode()) {
                 case Movie.REGULAR:
-                    thisAmount += 2;
+                    price += 2;
                     if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                        price += (each.getDaysRented() - 2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
+                    price += each.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
-                    thisAmount += 1.5;
+                    price += 1.5;
                     if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                        price += (each.getDaysRented() - 3) * 1.5;
                     break;
             }
 
@@ -51,8 +43,8 @@ public class Customer {
                 frequentRenterPoints++;
 
             result += "\t" + each.getMovie().getTitle() + "\t"
-                    + thisAmount + "\n";
-            totalAmount += thisAmount;
+                    + price + "\n";
+            totalAmount += price;
 
         }
 
@@ -61,6 +53,14 @@ public class Customer {
 
 
         return result;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String getName() {
+        return name;
     }
 
 }
